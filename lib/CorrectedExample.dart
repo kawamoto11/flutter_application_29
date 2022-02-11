8c53afbcbb2bbb6a9d82414b229146d6661f834d
hspoyo <po1poke@yahoo.co.jp>
2022-02-11 20:33:03 +0900

    あああああ

--- lib/CorrectedExample.dart
+++ lib/CorrectedExample.dart
@@ -33,24 +33,24 @@ class OrdinalComboBarLineChart extends StatelessWidget {
   /// Create series list with multiple series
   static List<charts.Series<OrdinalSales, String>> _createSampleData() {
     final desktopSalesData = [
-      OrdinalSales('2014', 5),
-      OrdinalSales('2015', 25),
-      OrdinalSales('2016', 100),
-      OrdinalSales('2017', 75),
+      OrdinalSales('2018', 5),
+      OrdinalSales('2019', 25),
+      OrdinalSales('2020', 100),
+      OrdinalSales('2021', 75),
     ];
 
     final tableSalesData = [
-      OrdinalSales('2014', 5),
-      OrdinalSales('2015', 25),
-      OrdinalSales('2016', 100),
-      OrdinalSales('2017', 85),
+      OrdinalSales('2018', 5),
+      OrdinalSales('2019', 25),
+      OrdinalSales('2020', 100),
+      OrdinalSales('2021', 75),
     ];
 
     final mobileSalesData = [
-      OrdinalSales('2014', 10),
-      OrdinalSales('2015', 50),
-      OrdinalSales('2016', 200),
-      OrdinalSales('2017', 150),
+      OrdinalSales('2018', 5),
+      OrdinalSales('2019', 25),
+      OrdinalSales('2020', 100),
+      OrdinalSales('2021', 75),
     ];
 
     return [
