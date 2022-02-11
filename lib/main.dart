52dc4c8a862543d7f61e55a22f334906b1be3b90
hspoyo <po1poke@yahoo.co.jp>
2022-02-11 20:35:06 +0900

    こんにちは

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('chart'),
+        title: Text('charts'),
       ),
       body: Container(
         child: ListView(
